e3c2ce448e0f4ffa3e4fe9053be9b79891ca7641
Olexandr Matviichuk <matviichuk.Olexandr@gmail.com>
2017-06-04 18:14:22 +0300

    refactor welcome screen in kv file

--- Libs/uix/__init__.py
+++ Libs/uix/__init__.py
@@ -3,6 +3,8 @@ from kivy.lang import Builder
 KV_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'main.kv'))
 Builder.load_file(KV_PATH)
 
+from nilearn import image
+
 from kivy.factory import Factory
 #from .jThemeView import JThemeView
 #Factory.register('JThemeView', JThemeView)
--- Libs/uix/weclomeScreen.py
+++ Libs/uix/weclomeScreen.py
@@ -5,32 +5,18 @@ from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 from kivy.properties import BooleanProperty
 from .jThemeView import JThemeView
-kvFileOpenExistSessionButtonID = "openExistSessionButtonID"
-kvFileCreateNewSessionButtonID = "createNewSessionButtonID"
+kvFileOpenExistSessionButtonID = "OpenExistSessionButtonID"
+kvFileCreateNewSessionButtonID = "CreateNewSessionButtonID"
 
 class WelcomeScreen(Screen):
-
-    def openExistSessionButtonPressed(self):
-        button = self.ids[kvFileOpenExistSessionButtonID]
-        button.background_color = [0.3, 0.3, 0.3, 1]
-        button.color = [1, 1, 1, 1]
 
 
     def openExistSessionButtonReleased(self):
         button = self.ids[kvFileOpenExistSessionButtonID]
-        button.background_color = [0.94, 0.94, 0.94, 1]
-        button.color = [0, 0, 0, 1]
-
-    def createNewSesionButtonPressed(self):
-        button = self.ids[kvFileCreateNewSessionButtonID]
-        button.background_color = [0.24, 0.54, 0.96, 1]
-        button.color = [1, 1, 1, 1]
 
     def createNewSessionButtonReleased(self):
         button = self.ids[kvFileCreateNewSessionButtonID]
-        button.background_color = [0, 0, 0, 0]
-        button.color = [0, 0, 0, 1]
-    pass
+
 
 class WelcomeScreenSessionListItemView(RecycleDataViewBehavior, Widget, JThemeView):
     filename = StringProperty()
