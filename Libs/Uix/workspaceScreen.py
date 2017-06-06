3981a1c70331fb42696bdd98675dcd565eb55cf5
Olexandr Matviichuk <matviichuk.Olexandr@gmail.com>
2017-06-06 16:06:23 +0300

    add binding for medialibraryTab on workspace screen

--- Libs/Uix/workspaceScreen.py
+++ Libs/Uix/workspaceScreen.py
@@ -1,7 +1,14 @@
 from kivy.uix.screenmanager import Screen
-
+from kivy.uix.textinput import TextInput
 
 class WorkspaceScreen(Screen):
+    def __init__(self, **kwargs):
+        super(WorkspaceScreen, self).__init__(**kwargs)
+        medialibrarySearchBar = self.ids["MediaLibrarySearchBar"]
+        medialibrarySearchBar.bind(on_search_text=self._filer_medialibrary_data)
+
+
+
     def new_session_button_pressed(self):
         print("new_session_button_pressed")
     def open_session_button_pressed(self):
@@ -10,4 +17,9 @@ class WorkspaceScreen(Screen):
         print("save_session_button_pressed")
     def add_to_library_button_pressed(self):
         print("add_to_library_button_pressed")
-    pass
+
+    def _filer_medialibrary_data(self, filterStr):
+
+        print(filterStr)
+        pass
+
